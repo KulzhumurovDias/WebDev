717fd9f9a73ecc3abcd430e7ab050f696f8c83a7
KulzhumurovDias <78645745+KulzhumurovDias@users.noreply.github.com>
2021-03-10 04:20:32 +0600

    Add files via upload

---
+++ Lab6/fake-db.ts
@@ -0,0 +1,14 @@
+import {Album} from './models';
+import {Photo} from './models';
+
+export const ALBUMS: Album [] = [
+  {id: 1, title: 'title1', body: 'Lorem Ipsum'},
+  {id: 2, title: 'title2', body: 'Lorem Ipsum'},
+  {id: 3, title: 'title3', body: 'Lorem Ipsum'},
+  {id: 4, title: 'title4', body: 'Lorem Ipsum'}
+];
+export const PHOTOS: Photo[] = [
+  {id: 1, title: 'title1', url: 'asda', thuUrl: 'asdasd'},
+  {id: 2, title: 'title2', url: 'asdaa', thuUrl: 'asdasdd'},
+  {id: 3, title: 'title3', url: 'asdaaa', thuUrl: 'asdasddd'},
+  ];
---
+++ Lab6/models.ts
@@ -0,0 +1,11 @@
+export interface Album{
+  id: number;
+  title: string;
+  body: string;
+}
+export interface Photo{
+  id: number;
+  title: string;
+  url: string;
+  thuUrl: string;
+}
